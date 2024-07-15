97749ec4af7b6113e22c43028ca9edff1e70eba7
Vineet Malewar <malewar.vineet2001@gmail.com>
2024-07-15 12:45:32 +0530

    adding functionality to buttons

--- src/AirlineManagementSystem/Login.java
+++ src/AirlineManagementSystem/Login.java
@@ -2,10 +2,16 @@ package AirlineManagementSystem;
 
 import javax.swing.*;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Login extends JFrame {
+public class Login extends JFrame implements ActionListener {
+
+    JButton btnReset,btnLogin, btnCancel;
+    JTextField tfUsername,pfPassword;
 
     public Login(){
         getContentPane().setBackground(Color.WHITE);
@@ -17,7 +23,7 @@ public class Login extends JFrame {
         lblUsername.setBounds(20, 20, 100,25);
         add(lblUsername);
 
-        JTextField tfUsername = new JTextField();
+        tfUsername = new JTextField();
         tfUsername.setBounds(120, 20, 200, 25);
         add(tfUsername);
 
@@ -26,28 +32,52 @@ public class Login extends JFrame {
         lblPassword.setBounds(20, 60, 100, 25);
         add(lblPassword);
 
-        JPasswordField pfPassword = new JPasswordField();
+        pfPassword = new JPasswordField();
         pfPassword.setBounds(120, 60, 200, 20);
         add(pfPassword);
 
         //adding buttons
-        JButton btnReset = new JButton("Reset");
+        btnReset = new JButton("Reset");
         btnReset.setBounds(60, 110, 100, 25);
+        btnReset.addActionListener(this);
         add(btnReset);
 
-        JButton btnLogin = new JButton("Login");
+        btnLogin = new JButton("Login");
         btnLogin.setBounds(230, 110, 100, 25);
+        btnLogin.addActionListener(this);
         add(btnLogin);
 
-        JButton btnCancle = new JButton("Cancle");
-        btnCancle.setBounds(150, 160, 100, 25);
-        add(btnCancle);
+        btnCancel = new JButton("Cancle");
+        btnCancel.setBounds(150, 160, 100, 25);
+        btnCancel.addActionListener(this);
+        add(btnCancel);
 
         setSize(400,250);
         setLocation(600,250);
         setVisible(true);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //adding functionality to buttons
+        if(e.getSource() == btnLogin){
+            //showing popup for login successful for testing
+            JOptionPane.showMessageDialog(null, "You have successfully logged in");
+
+        }else if(e.getSource() == btnCancel){
+            setVisible(false);
+        }else if(e.getSource() == btnReset){
+            tfUsername.setText("");
+            pfPassword.setText("");
+            JOptionPane.showMessageDialog(null, "Reset Successful");
+        }
+
+
+    }
+
     public static void main(String[] args) {
         new Login();
     }
+
+
 }
